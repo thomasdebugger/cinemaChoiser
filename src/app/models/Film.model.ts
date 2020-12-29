ae7062ec4be2f7250c9365672b80f1aab319a0d3
Thomas <thomas.ruggiero1403@gmail.com>
2020-12-29 12:59:25 +0100

    mise a jour des forms sous form-dynamique

---
+++ src/app/models/Film.model.ts
@@ -0,0 +1,11 @@
+export class Film {
+    constructor(public filmName: string, 
+                public realisator:string, 
+                public release:string, 
+                public actorList:string[], 
+                public filmCategory:string[],
+                public srcImg:string
+                ){
+
+    }
+}
